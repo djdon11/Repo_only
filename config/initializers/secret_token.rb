38901787e8c1fab4999647de4fe0da59efff26fe
Vaibhav Joshi <vaibhavjoshi@cdnsol.com>
2012-10-17 19:28:38 +0530

    Pushing to ski_resort

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SkiResorts::Application.config.secret_token = 'b487b6db72b79a8b51eca9f2d4522656e24baa77d47a63e71564e2176f6cf6837a6ed4259c5e7283eaf30454a4fa231b9c095aceece844f04184913efe92686a'
